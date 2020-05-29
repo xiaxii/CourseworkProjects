c4b167b6751f4df78d78037d46d97cd933045b68
xiaxii <xiaxi9889@gmail.com>
2020-05-29 08:53:34 +0800

    Deleted IntelliJ Project productions

--- BankSystem/JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"Account"},{"p":"<Unnamed>","l":"Account_Current"},{"p":"<Unnamed>","l":"Account_Junior"},{"p":"<Unnamed>","l":"Account_Saver"},{"p":"<Unnamed>","l":"Account.AccountStatus"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Customer"},{"p":"<Unnamed>","l":"GUI_BankSystem"},{"p":"<Unnamed>","l":"Account.Type"}]
